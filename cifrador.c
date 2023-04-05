e4c4ab2b799301a9b2b27ff60e58c47cc07e8fce
MTlls <matheustellestab8@gmail.com>
2023-04-05 00:02:17 -0300

    cria ArquivoDeChaves

--- cifrador.c
+++ cifrador.c
@@ -4,138 +4,60 @@
 
 #include "liblista.h"
 
-void insereChaves(FILE *livroCifra, lchar_t *listaChaves) {
-	wint_t c;
-	int i = 0;
-
-	/**
-	 * É inserido a primeira chave com o índice i
-	 */
-	c = fgetwc(livroCifra);
-	setChave(listaChaves, c, i);
-
-	while(c != WEOF) {
-		/**
-		 * Caso seja um espaço ou \n é pego o proximo caractere e inserido na lista de chaves
-		 * */
-		if(c == ' ' || c == '\n') {
-			/*É pego o próximo caracetere depois de ' ' ou '\n'*/
-			c = fgetwc(livroCifra);
-
-			/* Caso haja espaço no final do arquivo (não queremos um espaço nas chaves que iremos encriptografar) */
-			if(c == WEOF) {
-				return;
-			} else if(!setChave(listaChaves, c, ++i)) {  // Se ocorrer erro
-				fprintf(stderr, "ERRO: %s \"%c\": %s\n", "Nao foi possivel atualizar os indices do caractere ", c, strerror(errno));
-				exit(1);
-			}
-		}
-		c = fgetwc(livroCifra);
-	}
-}
-
 /**
  * Exclui espaços e quebras de linha em sequência, texto todo em minúsculo. Retorna um stream para o arquivo criado.
  */
-FILE *normalizaTexto(FILE *texto) {
+FILE *insereChaves(FILE *texto, lchar_t *listaChaves) {
+	// pneumoultramicroscopicossilicovulcanoconiótico possui 46 letras (maior palavra do alfabeto)
+	char palavra[46 + 1];
 	wint_t c;
 	FILE *arq;
-	char *fileName = "formatado.ascii";
+	int i;
+	char *fileName = "ArquivoDeChaves.ascii";
 
 	if(!(arq = fopen(fileName, "w"))) {
-		fprintf(stderr, "ERRO: %s \"%s\": %s\n", "Nao foi possivel abrir/criar o arquivo", fileName, strerror(errno));
-		exit(1);
+		return NULL;
 	}
 
-	c = fgetwc(texto);
+	// lê até encontrar ' ', '\t' ou  '\n'
+	for(i = 0; fscanf(texto, "%s", palavra) != EOF; i++) {
+		c = palavra[0];
+		c = iswupper(c) ? towlower(c) : c;
+		printf("%c-", c);
 
-	while(c != WEOF) {
-		// se for ' ' ou \n
-		if(iswspace(c)) {
-			fputwc(c, arq);
-			c = fgetwc(texto);
-			// enquanto houverem espaços ou quebras de linhas em sequencia, avança-se o cursor, cobrindo o caso no qual há linhas extras no final do arquivo
-			while(iswspace(c)) {
-				c = fgetwc(texto);
-			}
-			// tramamento caso o espaço seja seguido do fim do arquivo
-			if(c == WEOF) {
-				fclose(arq);
-				return fopen(fileName, "r");
-			}
+		if(!(setChave(listaChaves, c, i))) {
+			return NULL;
 		}
+		// atualiza o indice
+	};
+	printf("\n");
 
-		/* Se o caractere for maísculo, colocamos como minúsculo*/
-		if(iswupper(c)) {
-			c = towlower(c);
-		}
+	// escreve toda a lista de caracteres no arquivo e no stdout
+	lchar_imprime(arq, listaChaves);
 
-		fputwc(c, arq);
-		c = fgetwc(texto);
-	}
 	fclose(arq);
-
-	rewind(texto);
 
 	return fopen(fileName, "r");
 }
 
-/**
- * Retira os acentos do texto.
- */
-void retiraAcento(FILE *texto) {
-	wchar_t *comAcentos = L"ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
-	char semAcentos[] = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
-	wint_t c;
-	int i;
-
-	printf("ftell: %ld\n", ftell(texto));
-	fseek(texto, -sizeof(L'\0'), SEEK_END);
-	c = fgetwc(texto);
-
-	// mbtowc(&c, fgetc(texto), sizeof(wint_t));
-	if(!(c)) {
-		fprintf(stderr, "ERRO: %s: %s\n", "Nao foi possivel ler o caractere do arquivo", strerror(errno));
-		exit(1);
-	}
-
-	while(c != EOF) {
-		for(i = 0; i < wcslen(comAcentos); i++) {
-			if(c == comAcentos[i]) {
-				c = semAcentos[i];
-				/* é preciso sair do loop pois será feito a mesma verificação com o proximo caractere*/
-				i = wcslen(comAcentos);
-			}
-		}
-		fputwc(c, texto);
-		fseek(texto, -sizeof(c), SEEK_CUR);
-		c = getwc(texto);
-	}
-
-	rewind(texto);
-}
-
 FILE *cifrador(FILE *livroCifra) {
-	FILE *arqChaves, *flivroCifra;
+	FILE *arqChaves;
 	lchar_t *listaChar;
 
-	if(!(arqChaves = fopen("ArquivoDeChaves.txt", "w"))) {
-		fprintf(stderr, "ERRO: %s \"%s\": %s\n", "Nao foi possivel abrir/criar o arquivo", "ArquivoDeChaves.txt", strerror(errno));
-		exit(1);
-	}
-
+	/* Cria a lista necessária para guardar as chaves */
 	if(!(listaChar = lchar_cria())) {
 		fprintf(stderr, "ERRO: %s", "Falha ao criar lista de chaves.\n");
 		exit(1);
 	}
 
-	flivroCifra = normalizaTexto(livroCifra);
-	insereChaves(flivroCifra, listaChar);
+	/*Recebe a lista e cria o arquivo de chaves, caso ocorra imprevista realiza o devido tratamento do erro*/
 	printf("Lista: \n");
-	lchar_imprime(listaChar);
-	printf("------------------------------------------\n");
+	if(!(arqChaves = insereChaves(livroCifra, listaChar))) {
+		fprintf(stderr, "ERRO: %s \"%s\": %s\n", "Nao foi possivel abrir/criar o arquivo", "ArquivoDeChaves.ascii", strerror(errno));
+		exit(1);
+	}
+
 	lchar_destroi(listaChar);
 	fclose(arqChaves);
-	fclose(flivroCifra);
 	return arqChaves;
 }
--- cifrador.h
+++ cifrador.h
@@ -13,28 +13,17 @@
 /*
     Cria um arquivo com as chaves dadas pelo livroCifra
 */
-FILE* cifrador(FILE* livroCifra);
+FILE *cifrador(FILE *livroCifra);
 
 /*
  * Insere chaves e atualiza índices se a chave já existir.
  * Retorna 1 se inserido com sucesso, 0 caso contrário.
  */
-int setChave(lchar_t* l, int caractere, int indice);
+int setChave(lchar_t *l, int caractere, int indice);
 
 /**
- * Exclui espaços e quebras de linha em sequência, texto todo em minúsculo.
- * Retorna um stream para o arquivo criado.
+ * Insere as chaves obtidas do texto. Retorna com sucesso o arquivo ou NULL caso contrário.
  */
-FILE* normalizaTexto(FILE* texto);
-
-/*
- * Retira os acentos do texto.
- */
-void retiraAcento(FILE* livroCifra);
-
-/**
- * Insere as chaves obtidas do texto.
-*/
-void insereChaves(FILE* livroCifra, lchar_t* listaChaves);
+FILE* insereChaves(FILE *texto, lchar_t *listaChaves);
 
 #endif
